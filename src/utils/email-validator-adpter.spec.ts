402153a7665a9272ef271feffeee1cab609ef3d3
LuanBanhos <luan.p.banhos@outlook.com>
2022-11-19 03:01:28 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adpter.spec.ts
+++ src/utils/email-validator-adpter.spec.ts
@@ -17,10 +17,17 @@ test('Should return false is validator returns false',()=>{
     expect(isValid).toBe(false)
 })
 
-test('Should return true is validator returns false',()=>{
+test('Should return true is validator returns true',()=>{
     const sut = new EmailValidatorAdapter()
     const isValid= sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
+})
+
+test('Should call validator with correct email',()=>{
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy=jest.spyOn(validator,'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
 })
 
 
