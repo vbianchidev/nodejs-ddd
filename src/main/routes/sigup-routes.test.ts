ed0ce03b878f1d73a116ab8f6083c16f508e216a
LuanBanhos <luan.p.banhos@outlook.com>
2022-11-22 21:53:14 -0300

    refactor: add g api/signup

--- src/main/routes/sigup-routes.test.ts
+++ src/main/routes/sigup-routes.test.ts
@@ -1,12 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 
-// eslint-disable-next-line no-undef
 describe('SignUp Routes', () => {
-  // eslint-disable-next-line no-undef
   test('Should return an account on sucess', async () => {
     await request(app)
-      .post('/api/sigup')
+      .post('/api/signup')
       .send({
         name: 'Luan',
         email: 'luanBanhos@gmail.com',
